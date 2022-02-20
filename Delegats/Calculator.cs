d66f977ba2cafd73e2640b2f6ad29aa2efadfb31
Коновалова Анна Сергеевна <a.konovalova@action-media.ru>
2022-02-20 14:42:05 +0300

    summa,doubledivision,module

---
+++ Delegats/Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegats.Calculator
+{
+    public class CalculatorClass
+    {
+        public CalculatorClass()
+        {
+            _operationName = new Dictionary<string, string>()
+            {
+                { "+", "сложение" },
+                { "//", "двойное деление (x / y / y)" },
+                { "|/|", "возврат модуля от деления"}
+            };
+            
+            _operations = new Dictionary<string, OperationDelegate>()
+            {
+                { "+", Summa},
+                {"//", DoubleDivision},
+                {"|/|", ModuleDivision}
+            };
+        }
+
+
+        public Dictionary<string, string> _operationName;
+        
+        public delegate double OperationDelegate(double x, double y);
+        public Dictionary<string, OperationDelegate> _operations;
+
+
+        public void ShowAllOperations()
+        {
+            foreach (var operation in _operationName)
+            {
+                Console.WriteLine($"{operation.Key} - это {operation.Value}");
+            }
+        }
+        
+        public string GetOperationName(string op)
+        {
+            return _operationName[op];
+        }
+        
+        public void PerformOperation(string op, double x, double y)
+        {
+            //проверить введенную операцию
+            if (string.IsNullOrWhiteSpace(op))
+            {
+                throw new ArgumentException("не ввели символ");
+            }
+
+            if (!_operations.ContainsKey("op"))
+            {
+                throw new ArgumentException("нет такой операции");
+            }
+            
+            //проверить x и y
+            //
+            
+            //подсчет результата
+            var result = _operations[op](x, y);
+            
+            //вывод результата
+            Console.WriteLine($"result операции {_operationName[op]} равен {result}");
+        }
+
+        private double Summa(double x, double y)
+        {
+            return x + y;
+        }
+        
+        private double DoubleDivision(double x, double y)
+        {
+            var divisionResult = x / y;
+            return divisionResult / y;
+        }
+        
+        private double ModuleDivision(double x, double y)
+        {
+            var divisionResult = x / y;
+            return Math.Abs(divisionResult);
+        }
+    }
+}
---
+++ Delegats/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Delegats.Calculator;
+
+namespace Delegats
+{
+    class Program
+    {
+        private static CalculatorClass _calculatorClass = new CalculatorClass();
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Можно использовать калькулятор, он умеет:");
+            
+            _calculatorClass.ShowAllOperations();
+
+            var operationKey = Console.ReadLine();
+            var x = Convert.ToDouble(Console.ReadLine());
+            var y = Convert.ToDouble(Console.ReadLine());
+
+            _calculatorClass.PerformOperation(operationKey, x, y);
+
+            Console.ReadLine();
+        }
+
+        void ShowOperationInfo(string culture, string name)
+        {
+            var operationInfoRu = new Dictionary<string, string>()
+            {
+                { "сложение", "к 1му числу прибавить 2ое число" },
+                { "двойное деление (x / y / y)", "...." },
+                { "возврат модуля от деления", "поделить, потом взять модуль числа"}
+            };
+            var operationInfoUk = new Dictionary<string, string>()
+            {
+                { "сложение", "по-укр" },
+                { "двойное деление (x / y / y)", "...." },
+                { "возврат модуля от деления", "по-укр"}
+            };
+
+            if (culture == "Ru")
+            {
+                Console.WriteLine(operationInfoRu[name]);
+            }
+            else if (culture == "Uk")
+            {
+                Console.WriteLine(operationInfoUk[name]);
+            }
+            else if (culture == "En")
+            {
+                Console.WriteLine(operationInfoUk[name]);
+            }
+        }
+    }
+}
