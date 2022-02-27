efa486ce53542585eaccce8bc5625f56b7251106
Коновалова Анна Сергеевна <a.konovalova@action-media.ru>
2022-02-27 17:16:33 +0300

    task1

--- Delegats/Program.cs
+++ Delegats/Program.cs
@@ -1,38 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using Delegats.Calculator;
-using Delegats.Weapon;
+using Danya.Delegats.Services;
 
-namespace Delegats
+namespace Danya.Delegats
 {
     class Program
     {
-        private static WeaponClass _weaponClass = new WeaponClass();
-
-        private static CalculatorClass _calculatorClass = new CalculatorClass();
+        private static WeaponService _weaponService = new WeaponService();
+        private static CalculatorService _calculatorService = new CalculatorService();
         
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Можно использовать калькулятор, он умеет:");
-            
-            _calculatorClass.ShowAllOperations();
+            //служебная переменная
+            var operationKey = string.Empty;
 
-            var operationKey = Console.ReadLine();
+            _calculatorService.ShowAllOperations();
+            
+            
+            operationKey = Console.ReadLine();
             var x = Convert.ToDouble(Console.ReadLine());
             var y = Convert.ToDouble(Console.ReadLine());
-
-            _calculatorClass.PerformOperation(operationKey, x, y);
+            
+            _calculatorService.PerformOperation(operationKey, x, y);
 
             Console.WriteLine("Можно выбрать оружия, виды оружия есть:");
-            _weaponClass.ShowAllOperations();
+            _weaponService.ShowAllOperations();
 
             
-            var operationKey = Console.ReadLine();
-            var a = Convert.ToString(Console.ReadLine());
-            var b = Convert.ToString(Console.ReadLine());
+            operationKey = Console.ReadLine();
+            Console.WriteLine("Введите кол-во снарядов для стрельбы:");
+            var a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите кол-во проделанных выстрелов:");
+            var b = Convert.ToInt32(Console.ReadLine());
             
-             _weaponClass.PerformOperation(operationKey, x, y);
+             _weaponService.PerformOperation(operationKey, a, b);
             Console.ReadLine();
         }
 
